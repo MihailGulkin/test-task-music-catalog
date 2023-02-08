fe585be4ead62f82b45abf1046d1719eafa8d4a2
Bol4onok <95026332+MihailGulkin@users.noreply.github.com>
2023-02-09 01:43:05 +0300

    Added __str__ method in music:models

--- src/music/models.py
+++ src/music/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Artist(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f'{self.pk} - {self.name}'
+
 
 class Album(models.Model):
     artist = models.ForeignKey(
@@ -12,10 +15,16 @@ class Album(models.Model):
     )
     release_year = models.DateField()
 
+    def __str__(self):
+        return f'{self.pk} - {self.artist.name}'
+
 
 class Song(models.Model):
-    name = models.CharField(max_length=100)
-    album_id = models.ManyToManyField(
+    title = models.CharField(max_length=100)
+    albums_id = models.ManyToManyField(
         Album,
         blank=True
     )
+
+    def __str__(self):
+        return f'{self.pk} - {self.title}'
