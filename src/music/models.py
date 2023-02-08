e45fcd2d0f74b0b11e1422d084f273567c982a47
Bol4onok <95026332+MihailGulkin@users.noreply.github.com>
2023-02-08 22:17:48 +0300

    Create Artist, Album and Song models

--- src/music/models.py
+++ src/music/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Album(models.Model):
+    artist = models.ForeignKey(
+        Artist,
+        on_delete=models.CASCADE
+    )
+    release_year = models.DateField()
+
+
+class Song(models.Model):
+    name = models.CharField(max_length=100)
+    album_id = models.ManyToManyField(
+        Album,
+        blank=True
+    )
